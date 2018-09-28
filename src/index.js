83520074ce7af11dbd517fdf762c036adcbaa7a4
Ngoc Dong Pham <cpud1258@gmail.com>
2018-09-28 03:15:22 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,54 +1,25 @@
-// Code goes here
+// Code The Spaceship Component Here
 import React from 'react';
-import ReactDOM from 'react-dom';
+ class Spaceship extends React.Component {
+render(){
 
-class MovieCard extends React.Component {
-  render() {
-    // ... The render stuff from before
-    <div className="movie-card">
-<<<<<<< HEAD
-        <img
-          src={this.props.poster}
-          alt={this.props.title}
-=======
-        <img  
-          src={this.props.poster} 
-          alt={this.props.title} 
->>>>>>> e91d27fc669fa1d97a3a950058a1c17055b70440
-        />
-        <h2>{this.props.title}</h2>
-        <small>Genres: {this.props.genres.join(', ')}</small>
-      </div>
-  }
+return(
+  <p>
+  {this.props.name},
+  {this.props.speed},
+  {this.props.hasRockets},
+  {this.props.colors},
+
+  </p>)
 }
-<<<<<<< HEAD
 
-MovieCard.defaultProps = {
-  poster: 'http://i.imgur.com/bJw8ndW.png'
+
+
+ };
+Spaceship.defaultProps ={
+    name:'',
+    speed :'slow',
+    hasRockets :false,
+    colors:['black','red']
 };
-
-const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
-
-ReactDOM.render(
-  <MovieCard
-    title="Jurassic World"
-    genres={jurassicWorldGenres}
-  />,
-  document.getElementById('root')
-);
-=======
- 
-MovieCard.defaultProps = {
-  poster: 'http://i.imgur.com/bJw8ndW.png'
-};
- 
-const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
- 
-ReactDOM.render(
-  <MovieCard
-    title="Jurassic World"
-    genres={jurassicWorldGenres} 
-  />,
-  document.getElementById('root')
-);
->>>>>>> e91d27fc669fa1d97a3a950058a1c17055b70440
+export default Spaceship;
