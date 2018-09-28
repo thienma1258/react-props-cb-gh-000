62d89733936d68a6a3c2d1e261e9655db3c5e6e2
Ngoc Dong Pham <cpud1258@gmail.com>
2018-09-28 03:14:30 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -6,15 +6,22 @@ class MovieCard extends React.Component {
   render() {
     // ... The render stuff from before
     <div className="movie-card">
+<<<<<<< HEAD
         <img
           src={this.props.poster}
           alt={this.props.title}
+=======
+        <img  
+          src={this.props.poster} 
+          alt={this.props.title} 
+>>>>>>> e91d27fc669fa1d97a3a950058a1c17055b70440
         />
         <h2>{this.props.title}</h2>
         <small>Genres: {this.props.genres.join(', ')}</small>
       </div>
   }
 }
+<<<<<<< HEAD
 
 MovieCard.defaultProps = {
   poster: 'http://i.imgur.com/bJw8ndW.png'
@@ -29,3 +36,19 @@ ReactDOM.render(
   />,
   document.getElementById('root')
 );
+=======
+ 
+MovieCard.defaultProps = {
+  poster: 'http://i.imgur.com/bJw8ndW.png'
+};
+ 
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+ 
+ReactDOM.render(
+  <MovieCard
+    title="Jurassic World"
+    genres={jurassicWorldGenres} 
+  />,
+  document.getElementById('root')
+);
+>>>>>>> e91d27fc669fa1d97a3a950058a1c17055b70440
